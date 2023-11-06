edd64d6a7df2568bfffbca481898e52e5f3fc91f
Michael Ingold <michael.ingold@accenture.com>
2023-11-06 10:20:26 +0100

    added fsharp client libraries and controller sample

---
+++ samples/logistics/api/Program.cs
@@ -0,0 +1,27 @@
+using Manifesto.AspNet;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddManifesto()
+    .AddKeySpaces((string kind, string version, string group) => {
+        return  (kind, group, version) switch {
+            ("stock", "logistics.stockr.io", "v1alpha1") => $"/registry/stocks",
+            ("stocks", "logistics.stockr.io", "v1alpha1") => $"/registry/stocks",
+            ("location", "logistics.stockr.io", "v1alpha1") => $"/registry/locations",
+            ("locations", "logistics.stockr.io", "v1alpha1") => $"/registry/locations",
+            _ => string.Empty
+        };
+});
+
+var app = builder.Build();
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapManifestApiControllers();
+
+await app.RunAsync();
---
+++ samples/logistics/controller-fsharp/Program.fs
@@ -0,0 +1,87 @@
+﻿open System.Net.Http
+open System
+open stock
+open System.Threading
+open locations
+open api
+open FSharp.Control.Reactive
+
+// http client to access the Manifesto APIs
+let client = new HttpClient()
+client.BaseAddress <- new Uri("http://localhost:5000/apis/")
+
+// construct the stock and location APIs
+let stockApi =
+    api.ManifestsFor<StockSpecManifest> client "logistics.stockr.io/v1alpha1/stock"
+
+let locationsApi =
+    api.ManifestsFor<LocationSpecManifest> client "logistics.stockr.io/v1alpha1/location"
+
+let cts = new CancellationTokenSource()
+let sem = new SemaphoreSlim(0)
+
+// run the watch command that reads changes from the resource API
+let stocksWatch = stockApi.Watch cts.Token |> Async.RunSynchronously
+let stocks = stockApi.List
+let locationsWatch = locationsApi.Watch cts.Token |> Async.RunSynchronously
+let locations = locationsApi.List
+
+let appendToDict<'T when 'T :> Manifest> (d: Map<string, 'T>) (e: Event<'T>) =
+    match e with
+    | Update m -> d.Add(m.metadata.name, m)
+    | Create m -> d.Add(m.metadata.name, m)
+    | Delete m -> d.Remove(m.metadata.name)
+
+// Load the initial stocks and put them into a map with the name as the key
+let initialStockObs =
+    Subject.behavior (stocks |> Seq.map (fun x -> (x.metadata.name, x)) |> Map.ofSeq)
+// Merge the initial stocks map with the change stream
+let allStocks =
+    Observable.merge
+        (stocksWatch
+         |> Observable.scanInit (stocks |> Seq.map (fun x -> (x.metadata.name, x)) |> Map.ofSeq) appendToDict)
+        initialStockObs
+
+// Load the initial locations and put them into a map with the name as the key
+let initialLocationsObs =
+    Subject.behavior (locations |> Seq.map (fun x -> (x.metadata.name, x)) |> Map.ofSeq)
+// Merge the initial locations map with the change stream
+let allLocations =
+    Observable.merge
+        (locationsWatch
+         |> Observable.scanInit (locations |> Seq.map (fun x -> (x.metadata.name, x)) |> Map.ofSeq) appendToDict)
+        initialLocationsObs
+
+// compare the stocks and locations to find all stocks without a proper location.
+let x =
+    (Observable.combineLatest allLocations allStocks)
+        .Subscribe(
+            (fun (locs, stocks) ->
+                let locationIds = locs.Values |> Seq.map (fun x -> x.spec.Id)
+
+                let phantomStock =
+                    stocks.Values
+                    |> Seq.filter (fun x -> locationIds |> Seq.contains x.spec.Location |> not)
+                    |> Seq.map (fun x -> x.metadata.name)
+
+                printfn "Found phantom stock: %A" phantomStock),
+            (fun e -> 
+                eprintfn "failed: %A" e
+                cts.Cancel |> ignore
+                sem.Release |> ignore),
+            (fun () ->
+                printf "combiner completed"
+                cts.Cancel |> ignore
+                sem.Release |> ignore)
+        )
+
+Console.CancelKeyPress.Add(fun _ ->
+    printfn "Canceling watch"
+    cts.Cancel())
+
+async {
+    sem.WaitAsync() |> Async.AwaitTask |> Async.RunSynchronously
+    printfn "Shutting down..."
+
+}
+|> Async.RunSynchronously
